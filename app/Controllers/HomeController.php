b54b22199b79c8ded64c2a2556dad269a08fd3dc
felloz <serranol82@gmail.com>
2022-12-30 16:13:24 -0400

    techs repaired

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,11 +9,16 @@ class HomeController
 {
     public function show()
     {
-        $techs = [];
-        $techs1 = file_get_contents('http://localhost/public/assets/tech.json');
+        $techs  = [];
+        try {
+       
+        $techs1 = file_get_contents('assets/tech.json');
         $techs1 = json_decode($techs1);
         $techs[] = $techs1;
-        //dd($techs);
         return view('welcome', compact('techs'));
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+        
     }
 }
